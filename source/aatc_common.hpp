15cc20295487616834a00c1c82884969b6dcacd9
Sami Vuorela <samivuorela@gmail.com>
2015-10-03 16:41:14 +0300

    gc fix v1

--- source/aatc_common.hpp
+++ source/aatc_common.hpp
@@ -292,11 +292,10 @@ public:
 /*!\brief Basetype for script refcounted and GCd c++ objects to derive from.*/
 class aatc_refcounted_GC{
 public:
-	asIScriptEngine* gc_engine;
 	mutable int refCount;
 	mutable bool gcFlag;
 
-	aatc_refcounted_GC(asIScriptEngine* engine);
+	aatc_refcounted_GC();
 	virtual ~aatc_refcounted_GC();
 
 	void refcount_Add();
@@ -312,7 +311,9 @@ public:
 //refcounted and gc basetype
 
 
-bool aatc_templatecallback_container(asIObjectType *ot, bool &dontGarbageCollect);
+bool aatc_templatecallback_1tp(asIObjectType *ot, bool &dontGarbageCollect);
+bool aatc_templatecallback_map(asIObjectType *ot, bool &dontGarbageCollect);
+bool aatc_templatecallback_typeid(asIObjectType *ot, int typeId, bool &dontGarbageCollect);
 
 //call before engine destruction, as engine is supposed to automatically call this
 //because of SetEngineUserDataCleanupCallback(this function),
